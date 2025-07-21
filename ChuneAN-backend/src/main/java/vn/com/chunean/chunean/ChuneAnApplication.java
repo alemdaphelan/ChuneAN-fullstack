dca697487c071cba678ca4dd007e3ede4cba2180
hongocphuongnhu <phuongnhu25720055@gmail.com>
2025-07-21 19:48:43 +0700

    nhu

--- ChuneAN-backend/src/main/java/vn/com/chunean/chunean/ChuneAnApplication.java
+++ ChuneAN-backend/src/main/java/vn/com/chunean/chunean/ChuneAnApplication.java
@@ -7,9 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChuneAnApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(ChuneAnApplication.class, args);
-
+        System.out.println("Hello");
     }
 
 }
